20e3a3d52a46bfb4dca067168d5c21c438fb8f98
xiami <944314588@qq.com>
2015-07-17 10:26:36 +0800

    收藏平时开发中的比较好常用的工具类，代码片段。

---
+++ imike/src/main/java/com/xiami/imike/FileUploadListener.java
@@ -0,0 +1,5 @@
+package com.xiami.imike;
+
+public abstract interface FileUploadListener {
+	
+}
---
+++ imike/src/main/java/com/xiami/imike/SysConfig.java
@@ -0,0 +1,19 @@
+package com.xiami.imike;
+
+public class SysConfig {
+	
+	private static final SysConfig instance = new SysConfig();
+	
+	public static final int AABB = 10;
+	public static int aabb = 20;
+	private int aa = 30;
+	
+	
+	private SysConfig(){
+		
+	}
+	
+	
+	
+
+}
